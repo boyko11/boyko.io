b8c00a6a5697a5821d2d5cf79628aa916f0db6c7
Boyko Hadjiev-Yusev <boyko11@gmail.com>
2015-04-19 18:50:27 -0400

    More Email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,7 +2,6 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -14,9 +13,13 @@ $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+
+if($phone === NULL) {
+   $phone = 'no-phone';
+}
 	
 // Create the email and send the message
-$to = 'boyko11@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'boyko@boyko.io'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: admin@boyko.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
